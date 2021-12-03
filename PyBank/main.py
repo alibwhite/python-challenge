392ad445d24bc97ee567d7413d41e4f1b61f583e
alibwhite <81844382+alibwhite@users.noreply.github.com>
2021-12-03 22:10:34 +1100

    updated code

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,6 +4,30 @@ import csv
 budget_data_read = os.path.join("Resources/budget_data.csv")
 budget_data_write = os.path.join("Analysis/budget_analysis.txt")
 
+net_total_pf = 0
+total_months = 0
+change_pf = []
+change_month = []
+greatest_increase = ["", 0]
+greatest_decrease = ["", 999999999]
+
 with open(budget_data_read) as financial_info:
     reader=csv.reader(financial_info)
     header = next(reader)
+    first_row = next(reader)
+    
+
+
+output = (
+    f"Financial Analysis"
+    f"------------------"
+    f"Total Months: {total_months}"
+    f"Total: ${net_total}"
+    f"Average Change: ${average_change}"
+    f"Greatest Increase in Profits: {greatest_increase}"
+    f"Greatest Decrease in Profits: {greatest_decrease}"
+
+print(output)
+
+with open(budget_data_write, "w") as budget_summary:
+    budget_summary.write(output)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,3 +7,11 @@ poll_data_write = os.path.join("Analysis/election_analysis.txt")
 with open(poll_data_read) as election_info:
     reader=csv.reader(election_info)
     header = next(reader)
+
+
+
+
+print(output)
+
+with open(poll_data_write, "w") as poll_summary:
+    poll_summary.write(output)
