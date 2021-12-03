2c06b5d889b111d6d486f8b5e4bceb15f285d567
alibwhite <81844382+alibwhite@users.noreply.github.com>
2021-12-03 23:08:38 +1100

    completed pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,23 +9,43 @@ total_months = 0
 change_pf = []
 change_month = []
 greatest_increase = ["", 0]
-greatest_decrease = ["", 999999999]
+greatest_decrease = ["", 99999999999999999999999999999999999]
 
-with open(budget_data_read) as financial_info:
+with open(budget_data_read, "r") as financial_info:
     reader=csv.reader(financial_info)
     header = next(reader)
     first_row = next(reader)
-    
+    total_months = total_months + 1
+    net_total_pf = net_total_pf + int(first_row[1])
+    net_previous = int(first_row[1])
 
+    for row in reader:
+        total_months = total_months + 1
+        net_total_pf = net_total_pf + int(row[1])
+        net_change = int(row[1]) - net_previous
+        net_previous = int(row[1])
+        change_pf = change_pf + [net_change]
+        change_month = change_month + [row[0]]
+
+        if net_change < greatest_decrease[1]:
+            greatest_decrease[0] = row[0]
+            greatest_decrease[1] = net_change
+        
+        if net_change > greatest_increase[1]:
+            greatest_increase[0] = row[0]
+            greatest_increase[1] = net_change
+
+average_net_change = sum(change_pf)/len(change_pf)
 
 output = (
-    f"Financial Analysis"
-    f"------------------"
-    f"Total Months: {total_months}"
-    f"Total: ${net_total}"
-    f"Average Change: ${average_change}"
-    f"Greatest Increase in Profits: {greatest_increase}"
-    f"Greatest Decrease in Profits: {greatest_decrease}"
+    f"Financial Analysis\n"
+    f"------------------\n"
+    f"Total Months: {total_months}\n"
+    f"Total: ${net_total_pf}\n"
+    f"Average Change: ${average_net_change}\n"
+    f"Greatest Increase in Profits: {greatest_increase}\n"
+    f"Greatest Decrease in Profits: {greatest_decrease}\n"
+    )
 
 print(output)
 
